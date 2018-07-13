91e777a773cd032edb0023d0b93d82835208c8e7
Matthias Jenny <matthiascjenny@gmail.com>
2018-07-12 22:31:05 -0400

    Make component ES5

--- frontend/components/some_component.jsx
+++ frontend/components/some_component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-class SomeComponent extends React.Component {
-  render() {
-    return <h1>Some Component</h1>;
-  }
+function SomeComponent(props) {
+  React.Component.call(this, props);
 }
+
+SomeComponent.prototype = Object.create(React.Component.prototype);
+SomeComponent.prototype.constructor = SomeComponent;
+
+SomeComponent.prototype.render = function() {
+  return <h1>Some Component</h1>;
+};
 
 export default SomeComponent;
